8e27fa2b8bc6f6ae8afc08c82036074b5c0220a7
Marlon Valencia <marlondvalencia@gmail.com>
2023-11-02 19:11:09 -0500

    External API CALL working properly

--- src/main/java/com/foreign/foreignexchange/domain/controllers/ForeignExchangeController.java
+++ src/main/java/com/foreign/foreignexchange/domain/controllers/ForeignExchangeController.java
@@ -1,4 +1,20 @@
 package com.foreign.foreignexchange.domain.controllers;
 
+import com.foreign.foreignexchange.domain.generic.Currency;
+import com.foreign.foreignexchange.domain.services.ExternalApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/foreign-exchange")
 public class ForeignExchangeController {
+    @Autowired
+    private ExternalApiService externalApiService;
+
+    @GetMapping("")
+    public Currency obtenerData(){
+        return externalApiService.getCurrency();
+    }
 }
